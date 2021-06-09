bccafe17b9f40e988d67f20eef0d5f1d9ead6897
Zipeng Liu <zipeng@cs.ubc.ca>
2021-06-09 22:06:14 +0800

    small fixes

--- corgie-ui/src/components/NodePairList.js
+++ corgie-ui/src/components/NodePairList.js
@@ -2,13 +2,14 @@ import React, { Component, memo, useCallback } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Button, Badge } from "react-bootstrap";
+import cn from "classnames";
 import { FixedSizeList } from "react-window";
 import debounce from "lodash.debounce";
 import { selectNodePair, highlightNodePairs, hoverNode } from "../actions";
 
 export class NodePairList extends Component {
     render() {
-        const { nodes, highlightedNodePairs } = this.props;
+        const { nodes, highlightedNodePairs, selectedPairIdx } = this.props;
         const { selectNodePair, highlightNodePairs } = this.props;
 
         const labelOrId = nodes && nodes[0].label ? "label" : "id";
@@ -17,7 +18,7 @@ export class NodePairList extends Component {
             const debouncedHover = useCallback(debounce((x) => this.props.hoverNode(x), 200));
             return (
                 <div
-                    className="list-group-item"
+                    className={cn("list-group-item", { selected: index === selectedPairIdx })}
                     onMouseEnter={debouncedHover.bind(this, [p[0], p[1]])}
                     onMouseLeave={debouncedHover.bind(this, null)}
                     onClick={selectNodePair.bind(null, p[0], p[1])}
@@ -66,9 +67,24 @@ export class NodePairList extends Component {
     }
 }
 
+const getSelectedPairIdx = (pairs, selectedNodes) => {
+    if (selectedNodes.length !== 2 || selectedNodes[0].length !== 1 || selectedNodes[1].length !== 1)
+        return -1;
+    const s1 = selectedNodes[0][0],
+        s2 = selectedNodes[1][0];
+    for (let i = 0; i < pairs.length; i++) {
+        const p = pairs[i];
+        if ((p[0] === s1 && p[1] === s2) || (p[0] === s2 && p[1] === s1)) {
+            return i;
+        }
+    }
+    return -1;
+};
+
 const mapStateToProps = (state) => ({
     nodes: state.graph.nodes,
     highlightedNodePairs: state.highlightedNodePairs,
+    selectedPairIdx: getSelectedPairIdx(state.highlightedNodePairs, state.selectedNodes),
 });
 
 const mapDispatchToProps = (dispatch) =>
